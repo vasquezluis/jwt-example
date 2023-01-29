99e2342a597678eddeaf780f423131257b5ba062
LuiiVasquez <luivasque95@gmail.com>
2023-01-28 20:26:04 -0600

    adding token verification

--- src/middlewares/verifyToken.middlewares.js
+++ src/middlewares/verifyToken.middlewares.js
@@ -1,16 +1,41 @@
 // Authorization: Bearer <token>
-export const verifyToken = (req, res, next) => {
+
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const secret = process.env.SECRET;
 
 /**
- * * Obtener token almacenado en headers para autorizacion
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
  */
+export const verifyToken = (req, res, next) => {
+  /**
+   * * Obtener token almacenado en headers para autorizacion
+   */
 
   const bearerHeader = req.headers["authorization"];
 
   if (typeof bearerHeader !== "undefined") {
     const bearerToken = bearerHeader.split(" ")[1];
     req.token = bearerToken;
-    next();
+
+    try {
+      jwt.verify(req.token, secret, (error, authData) => {
+        if (error) {
+          console.log(error.message);
+          res.status(403).json({ error: error.message });
+        } else {
+          // console.log({ message: "authData", authData });
+          next();
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   } else {
     res.sendStatus(403);
   }
